3ee5bd5708c803975e36a472bb9532dd26f7b95a
dnunez10 <diana_nunez10@yahoo.com>
2021-03-24 14:14:25 -0700

    Done.

--- sushi-saga-client/src/App.js
+++ sushi-saga-client/src/App.js
@@ -7,11 +7,63 @@ const API = "http://localhost:3000/sushis"
 
 class App extends Component {
 
+  state = {
+    sushi: [],
+    index: 0,
+    wallet: 200,
+    message: null
+  }
+
+  componentDidMount(){
+    fetch(API)
+    .then(res => res.json())
+    .then(sushi => this.setState({sushi}))
+  }
+
+  getFourSushi(){
+    const fourSushi = this.state.sushi.slice(this.state.index, this.state.index + 4)
+    return fourSushi
+  }
+
+  updateIndex = () => {
+    this.setState({
+      index: this.state.index + 4
+    })
+  }
+
+  eatSushi = (sushiId, sushiPrice) => {
+    if (this.state.wallet >= sushiPrice){
+      const newSushiArray = this.state.sushi.map( (s) => {
+        if (s.id === sushiId) {
+          return { ...s, isEaten: true}
+        }else{
+          return s
+        }
+      })
+
+      this.setState({
+        sushi: newSushiArray,
+        wallet: this.state.wallet - sushiPrice,
+      })
+    } else {
+      this.setState({ message: 'no more money'})
+    }
+  }
+  eatenSushi = () => {
+    return this.state.sushi.filter((s) => s.isEaten)   
+}
+  
+
+
   render() {
     return (
       <div className="app">
-        <SushiContainer />
-        <Table />
+        <SushiContainer 
+        fourSushi={this.getFourSushi()} 
+        moreSushi={this.updateIndex} 
+        eatSushi={this.eatSushi}
+        />
+        <Table eatenSushi={this.eatenSushi()} wallet={this.state.wallet}/>
       </div>
     );
   }
--- sushi-saga-client/src/components/Sushi.js
+++ sushi-saga-client/src/components/Sushi.js
@@ -1,20 +1,22 @@
 import React, { Fragment } from 'react'
 
 const Sushi = (props) => {
+  const {sushi: {id, name, img_url, price, isEaten}, eatSushi} = props
+  
   return (
     <div className="sushi">
       <div className="plate" 
-           onClick={/* Give me a callback! */ null}>
+           onClick={() => eatSushi(id, price)}>
         { 
           /* Tell me if this sushi has been eaten! */ 
-          false ?
+          isEaten ?
             null
           :
-            <img src={/* Give me an image source! */ } width="100%" />
+            <img src={img_url} width="100%" />
         }
       </div>
       <h4 className="sushi-details">
-        {/* Give me a name! */} - ${/* Give me a price! */}
+        {name} - ${price}
       </h4>
     </div>
   )
